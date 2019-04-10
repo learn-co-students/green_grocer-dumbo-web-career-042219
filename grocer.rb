baa6acf4dd5a6526897e5ce862b4e44cd8e6da6d
John Hill <john.m.hill91@gmail.com>
2019-04-10 06:05:17 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,52 @@
 def consolidate_cart(cart)
-  # code here
+  consolCart={}
+  cart.each do |product|
+    product.each do |productName, details|
+      if consolCart[productName] == nil 
+        consolCart[productName]= details
+        consolCart[productName][:count]=1 
+      else 
+      consolCart[productName][:count]+= 1 
+    end 
+  end
 end
+consolCart
+end 
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    if cart.key?(coupon[:item])
+      couponCount = 0
+      until coupon[:num] > cart[coupon[:item]][:count]
+        cart[coupon[:item]][:count] -= coupon[:num]
+        cart["#{coupon[:item]} W/COUPON"] = {price: coupon[:cost], clearance: cart[coupon[:item]][:clearance], count: couponCount += 1}
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |product, details|
+      if details[:clearance] == true 
+        details[:price]= (details[:price]*0.80).round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  consolCart=consolidate_cart(cart)
+  coupCart=apply_coupons(consolCart, coupons)
+  finalCart=apply_clearance(coupCart)
+  cartTotal=0 
+  
+  finalCart.each do |product, detail|
+    cartTotal += detail[:price]* detail[:count]
+  end
+  
+  if cartTotal > 100
+    cartTotal= (cartTotal * 0.9)
+  end
+  cartTotal
 end
